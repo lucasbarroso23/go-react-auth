5d7a259a2aab3214020432201dad7cc4e09eb4f8
lucasbarroso23 <lucasbarroso23@gmail.com>
2022-09-02 20:02:43 -0700

    cors middleware

--- backend/app/router.go
+++ backend/app/router.go
@@ -1,8 +1,24 @@
 package app
 
-import "github.com/lucasbarroso23/go-react-auth/backend/controller/users"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/lucasbarroso23/go-react-auth/backend/controller/users"
+)
 
 func mapUrls() {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	router.POST("/api/register", users.Register)
 	router.POST("/api/login", users.Login)
 	router.GET("/api/user", users.Get)
