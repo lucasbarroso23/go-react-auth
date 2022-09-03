c81113b63e2bc5050320d4ce27dced17cc5d6599
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2022-09-03 16:35:38 -0300

    react app setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello react</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
